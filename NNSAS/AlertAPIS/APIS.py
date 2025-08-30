a3f071a4ac048efd57420a8c5b496328ddab1c8a
Nox <86739524+Noxxinoxx@users.noreply.github.com>
2025-08-30 23:07:09 +0000

    added info data in api

--- NNSAS/AlertAPIS/APIS.py
+++ NNSAS/AlertAPIS/APIS.py
@@ -1,7 +1,8 @@
-
-
-
+#APIS
+#Apis handles all the connected apis
+#it can be calender api or a test data api.
+#it will also handle securing the data.
 
 class API:
     def __init__(self):
-        self.api = 
+        self.api = ''
