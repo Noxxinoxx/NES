0bc03c0fc953698016e15dfd37676e6ba5dfa16c
Nox <86739524+Noxxinoxx@users.noreply.github.com>
2025-09-15 00:10:31 +0000

    Added some code to health controller go scripten

--- workers/health_controller_worker/main.go
+++ workers/health_controller_worker/main.go
@@ -1,5 +1,11 @@
 package main
 
+func check_health() {
+	print("this is my health check.")
+	print("new line here the test is running in portainer")
+
+}
+
 func main() {
 	print("health is running")
 }
